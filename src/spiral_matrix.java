d8093e438031c8ba223c228320eb95c8f76fb2aa
heet aghera <heetaghera809@gmail.com>
2023-03-25 00:01:41 +0530

    create a 2d array and basic operation of it

---
+++ src/spiral_matrix.java
@@ -0,0 +1,31 @@
+public class spiral_matrix {
+    public static void spiral(int[][]arr ){
+        int StartRow=0;
+        int EndRow=arr.length-1;
+        int StartCol=0;
+        int EndCol=arr[0].length-1;
+        while(StartRow<=EndRow && StartCol<=EndCol){
+
+            for(int j=StartCol;j<=EndCol;j++){
+                System.out.println(arr[StartRow][j]);
+            }
+            for(int i=StartRow+1;i<=EndRow;i++){
+                System.out.println(arr[EndCol][i]);
+            }
+            for(int j=EndCol-1;j<=StartCol;j--){
+                System.out.println(arr[EndRow][j]);
+            }
+            for(int i=EndRow-1;i<=StartRow+1;i++){
+                System.out.println(arr[i][StartCol]);
+            }
+        }
+        System.out.println();
+    }
+    public static void main(String[]args){
+        int [][]arr={{1,2,3,4},
+                     {5,6,7,8},
+                     {9,10,11,12},
+                     {13,14,15,16}};
+        spiral(arr);
+    }
+}
