73ddb3882b840dae908c5488ce8508d05061be18
heet aghera <heetaghera809@gmail.com>
2023-03-26 10:19:32 +0530

    Adding code for diagonal sum of 2D array

---
+++ src/DiagonalSum_TwoDArray.java
@@ -0,0 +1,22 @@
+public class DiagonalSum_TwoDArray {
+    public static void basicmethod(int [][]arr){    //here we are calculate the sum of primary and secondary both diagonal's element
+        int sum=0;
+        for(int i=0;i< arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(i==j){
+                    sum+=arr[i][j];
+                }
+                else if(i+j==arr.length-1){
+                    sum+=arr[i][j];
+                }
+            }
+        }
+        System.out.println("sum = "+sum);
+    }
+    public static void main(String[]args){
+        int [][]arr={{1,2,3},
+                     {4,5,6},
+                     {7,8,9}};
+        basicmethod(arr);
+    }
+}
