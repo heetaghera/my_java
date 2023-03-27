c07002f4a81826a6084c3e6f7931f21ea9cbb5b4
heet aghera <heetaghera809@gmail.com>
2023-03-27 23:54:15 +0530

    there are 2 codes for:-1.compare strings                        2.find the easiest path from all direction

--- src/string_compare.java
+++ src/string_compare.java
@@ -1,4 +1,33 @@
 public class string_compare {
+//    code for compare strings
+    public static void compare(){
+        String s1="heet";
+        String s2="heet";
+        String s3=new String("heet");
+
+        if(s1==s2){
+            System.out.println("s1 and s2 are same");
+        }
+        else{
+            System.out.println("s1 and s2 are not same");
+        }
+
+        if(s1==s3){
+            System.out.println("s1 and s3 are same");
+        }
+        else{
+            System.out.println("s1 and s3 are not same");
+        }
+
+        if(s1.equals(s3)){
+            System.out.println("s1 and s3 are same");
+        }
+        else{
+            System.out.println("s1 and s2 are same");
+        }
+    }
+
+    //code for find the shortest direction of path
     public static float path(String path){
         int x=0;
         int y=0;
@@ -24,5 +53,6 @@ public class string_compare {
     public static void main(String[]args){
         String path="WNEENESENNN";
         System.out.println(path(path));
+        compare();
     }
 }
