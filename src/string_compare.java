6b578c149234ad8615a6822bfad44dd0cc0782e3
heet aghera <heetaghera809@gmail.com>
2023-03-27 23:34:20 +0530

    code for search an element in sorted array

---
+++ src/string_compare.java
@@ -0,0 +1,28 @@
+public class string_compare {
+    public static float path(String path){
+        int x=0;
+        int y=0;
+        for(int i=0;i<path.length();i++){
+            int dir=path.charAt(i);
+            if(dir=='N'){
+               y++;
+            }
+            else if(dir=='S'){
+                y--;
+            }
+           else if(dir=='E'){
+                x++;
+            }
+           else {
+                x--;
+            }
+        }
+        int x2=x*x;
+        int y2=y*y;
+        return((float)Math.sqrt(x2+y2));
+    }
+    public static void main(String[]args){
+        String path="WNEENESENNN";
+        System.out.println(path(path));
+    }
+}
